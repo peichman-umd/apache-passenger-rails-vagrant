d3f69ad3033c4b48440b8760b66bbbbab07f1c00
Peter Eichman <peichman@umd.edu>
2016-03-29 11:41:27 -0400

    Ensure Apache starts at boot.

--- scripts/passenger.sh
+++ scripts/passenger.sh
@@ -7,3 +7,6 @@ curl --fail -sSLo /etc/yum.repos.d/passenger.repo \
   https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
 yum install -y mod_passenger
 systemctl restart httpd
+
+# ensure Apache starts at boot
+systemctl enable httpd.service
